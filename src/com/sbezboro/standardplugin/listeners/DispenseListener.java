4ce12a75866dfb58ecc85b9aaa4f248e388a6881
apv1313 <apv1313@users.noreply.github.com>
2016-08-03 23:15:31 +0200

    Prevent destruction of end portals by dispensers

--- src/com/sbezboro/standardplugin/listeners/DispenseListener.java
+++ src/com/sbezboro/standardplugin/listeners/DispenseListener.java
@@ -2,6 +2,10 @@ package com.sbezboro.standardplugin.listeners;
 
 import com.sbezboro.standardplugin.StandardPlugin;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.material.Dispenser;
+import org.bukkit.material.MaterialData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
@@ -16,6 +20,15 @@ public class DispenseListener extends EventListener implements Listener {
 	public void onBlockDispense(BlockDispenseEvent event) {
 		if (event.getItem().getType() == Material.LAVA_BUCKET) {
 			event.setCancelled(true);
+		}
+		
+		if (event.getItem().getType() == Material.WATER_BUCKET) {
+			MaterialData facingData = event.getBlock().getState().getData();
+			BlockFace face = ((Dispenser)facingData).getFacing();
+			Block targetBlock = event.getBlock().getRelative(face);
+			if (targetBlock.getType() == Material.ENDER_PORTAL) {
+				event.setCancelled(true);
+			}
 		}
 	}
 
