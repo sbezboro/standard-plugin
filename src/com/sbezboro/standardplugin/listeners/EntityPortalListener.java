52c58b0a63d6bc8465eb44203d0722b2e487275f
tincon <tincon1mc@gmail.com>
2019-02-11 08:24:40 -0600

    Disabled TNT carts unable to go through portals Disabled Primed TNT through portals

--- src/com/sbezboro/standardplugin/listeners/EntityPortalListener.java
+++ src/com/sbezboro/standardplugin/listeners/EntityPortalListener.java
@@ -2,6 +2,7 @@ package com.sbezboro.standardplugin.listeners;
 
 import com.sbezboro.standardplugin.StandardPlugin;
 import org.bukkit.Location;
+import org.bukkit.PortalType;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -9,18 +10,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPortalEvent;
 
 public class EntityPortalListener extends EventListener implements Listener {
-	public EntityPortalListener(StandardPlugin plugin) {
-		super(plugin);
-	}
+    public EntityPortalListener(StandardPlugin plugin) {
+        super(plugin);
+    }
 
-	@EventHandler(ignoreCancelled = true)
-	public void onEntityPortalEnter(EntityPortalEvent event) {
-		EntityType entityType = event.getEntityType();
-		Location location = event.getFrom();
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityPortalEnter(EntityPortalEvent event) {
+        EntityType entityType = event.getEntityType();
+        Location location = event.getFrom();
 
-		if (location.getWorld().getEnvironment() == World.Environment.NORMAL &&
-				entityType == EntityType.PIG_ZOMBIE) {
-			event.setCancelled(true);
-		}
-	}
+        if (location.getWorld().getEnvironment() == World.Environment.NORMAL &&
+                entityType == EntityType.PIG_ZOMBIE || entityType == EntityType.MINECART_TNT || entityType == EntityType.PRIMED_TNT) {
+            event.setCancelled(true);
+        }
+    }
 }
