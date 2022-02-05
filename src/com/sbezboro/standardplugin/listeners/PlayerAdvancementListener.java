1203770fa13c36384920cf82ced5753acf309837
dr-hagar <hohohogar@gmail.com>
2022-02-05 23:53:30 +0100

    advancement listener, send info text to webchat

--- src/com/sbezboro/standardplugin/listeners/PlayerAdvancementListener.java
+++ src/com/sbezboro/standardplugin/listeners/PlayerAdvancementListener.java
@@ -29,7 +29,7 @@ public class PlayerAdvancementListener extends EventListener implements Listener
 		String advancement_key = event.getAdvancement().getKey().getKey();
 		if (!advancement_key.startsWith("recipes")) {
 			String advancement_text = advancement_key.substring(advancement_key.indexOf('/') + 1).replaceAll("_", " ");
-			StandardPlugin.webchatMessage(String.format("%s%s%s got the advancement [%s%s]", ChatColor.AQUA, player.getDisplayName(), ChatColor.WHITE, ChatColor.GREEN, advancement_text));
+			StandardPlugin.webchatMessage(String.format("%s%s%s got the advancement %s[%s]", ChatColor.AQUA, player.getDisplayName(), ChatColor.WHITE, ChatColor.GREEN, advancement_text));
 		}
 	}
 }
